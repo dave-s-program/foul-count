4ce85e5a05db6133db145cd2398399e26244627e
Dave <malhindave@gmail.com>
2019-12-16 13:46:43 +0800

    Updates

---
+++ precache-manifest.cecf99fd710f662479c2731fe93ad985.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57f801bbed4fc9c18451",
+    "url": "/foul-count/static/css/main.b8ce74e0.chunk.css"
+  },
+  {
+    "revision": "57f801bbed4fc9c18451",
+    "url": "/foul-count/static/js/main.ff06ace1.chunk.js"
+  },
+  {
+    "revision": "50bc5eb0bd81c71954ed",
+    "url": "/foul-count/static/js/runtime~main.c46a4273.js"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/foul-count/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "8a605405b0e256c5d1fa28feef04e3b8",
+    "url": "/foul-count/index.html"
+  }
+];
